a6bdd9113103dbdf1272ab6d66e51066ea3f347b
Chandra Irugalbandara <irugalbandarachandra@gmail.com>
2020-08-01 04:57:57 +0530

    calculate screen viewmodel done

--- currency_converter_app/lib/business_logic/view_models/calculate_screen_viewmodel.dart
+++ currency_converter_app/lib/business_logic/view_models/calculate_screen_viewmodel.dart
@@ -15,7 +15,7 @@ class CalculateScreenViewModel extends ChangeNotifier {
   static final CurrencyPresentation defaultBaseCurrency = CurrencyPresentation(
       flag: '', alphabeticCode: '', longName: '', amount: '');
 
-  void LoadData() async {
+  void loadData() async {
     await _loadCurrencies();
     _rates = await _currencyService.getAllExchangeRates(
         base: _baseCurrency.alphabeticCode);
@@ -28,9 +28,88 @@ class CalculateScreenViewModel extends ChangeNotifier {
     _quoteCurrencies = _loadQuoteCurrencies(currencies);
   }
 
-  CurrencyPresentation _loadBaseCurrency(List<Currency> currencies) {}
+  CurrencyPresentation _loadBaseCurrency(List<Currency> currencies) {
+    if (currencies.length == 0) {
+      return defaultBaseCurrency;
+    }
+    String code = currencies[0].isoCode;
+    return CurrencyPresentation(
+        flag: IsoData.flagOf(code),
+        alphabeticCode: code,
+        longName: IsoData.longNameOf(code),
+        amount: ''
+    );
+  }
 
-  List<CurrencyPresentation> _loadQuoteCurrencies(List<Currency> currencies) {}
+  List<CurrencyPresentation> _loadQuoteCurrencies(List<Currency> currencies) {
+    List<CurrencyPresentation> quotes = [];
+    for (int i = 1; i < currencies.length; i++) {
+      String code = currencies[i].isoCode;
+      quotes.add(CurrencyPresentation(
+        flag: IsoData.flagOf(code),
+        alphabeticCode: code,
+        longName: IsoData.longNameOf(code),
+        amount: currencies[i].amount.toStringAsFixed(2),
+      ));
+    }
+    return quotes;
+  }
+  
+  CurrencyPresentation get baseCurrency {
+    return _baseCurrency;
+  }
+
+  List<CurrencyPresentation> get quoteCurrencies {
+    return _quoteCurrencies;
+  }
+
+  void calculateExchange(String baseAmount) async {
+    double amount;
+    try {
+      amount = double.parse(baseAmount);
+    } catch (e) {
+      _updateCurrenciesFor(0);
+      notifyListeners();
+      return null;
+    }
+
+    _updateCurrenciesFor(amount);
+
+    notifyListeners();
+  }
+
+  void _updateCurrenciesFor(double baseAmount) {
+    for (CurrencyPresentation c in _quoteCurrencies) {
+      for (Rate r in _rates) {
+        if (c.alphabeticCode == r.quoteCurrency) {
+          c.amount = (baseAmount * r.exchangeRate).toStringAsFixed(2);
+          break;
+        }
+      }
+    }
+  }
+
+  Future refreshFavorites() async {
+    await _loadCurrencies();
+    notifyListeners();
+  }
+
+  Future setNewBaseCurrency(int quoteCurrencyIndex) async {
+    _quoteCurrencies.add(_baseCurrency);
+    _baseCurrency = _quoteCurrencies[quoteCurrencyIndex];
+    _quoteCurrencies.removeAt(quoteCurrencyIndex);
+    await _currencyService.saveFavoriteCurrencies(_convertPresentationToCurrency());
+    loadData();
+  }
+
+  List<Currency> _convertPresentationToCurrency() {
+    List<Currency> currencies = [];
+    currencies.add(Currency(_baseCurrency.alphabeticCode));
+    for (CurrencyPresentation currency in _quoteCurrencies) {
+      currencies.add(Currency(currency.alphabeticCode));
+    }
+    return currencies;
+  }
 }
 
 class CurrencyPresentation {
