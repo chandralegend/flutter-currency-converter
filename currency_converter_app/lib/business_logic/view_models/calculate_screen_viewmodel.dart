743631b68a59df718b5d71f675ba78c4cf489831
Chandra Irugalbandara <irugalbandarachandra@gmail.com>
2020-08-01 03:49:31 +0530

    clalculate screen view model halfway

--- currency_converter_app/lib/business_logic/view_models/calculate_screen_viewmodel.dart
+++ currency_converter_app/lib/business_logic/view_models/calculate_screen_viewmodel.dart
@@ -7,4 +7,38 @@ import 'package:currency_converter_app/services/service_locator.dart';
 
 class CalculateScreenViewModel extends ChangeNotifier {
   final CurrencyService _currencyService = serviceLocator<CurrencyService>();
+
+  CurrencyPresentation _baseCurrency = defaultBaseCurrency;
+  List<CurrencyPresentation> _quoteCurrencies = [];
+  List<Rate> _rates = [];
+
+  static final CurrencyPresentation defaultBaseCurrency = CurrencyPresentation(
+      flag: '', alphabeticCode: '', longName: '', amount: '');
+
+  void LoadData() async {
+    await _loadCurrencies();
+    _rates = await _currencyService.getAllExchangeRates(
+        base: _baseCurrency.alphabeticCode);
+    notifyListeners();
+  }
+
+  Future<void> _loadCurrencies() async {
+    final currencies = await _currencyService.getFavoriteCurrencies();
+    _baseCurrency = _loadBaseCurrency(currencies);
+    _quoteCurrencies = _loadQuoteCurrencies(currencies);
+  }
+
+  CurrencyPresentation _loadBaseCurrency(List<Currency> currencies) {}
+
+  List<CurrencyPresentation> _loadQuoteCurrencies(List<Currency> currencies) {}
+}
+
+class CurrencyPresentation {
+  final String flag;
+  final String alphabeticCode;
+  final String longName;
+  String amount;
+
+  CurrencyPresentation(
+      {this.flag, this.alphabeticCode, this.amount, this.longName});
 }
